aa2e8175b9077181a75d1c61406c9aa11cfa66e6
manan0017 <manankakadiya007@gmail.com>
2022-03-24 13:40:18 +0530

    - Project initialize

---
+++ Edvora_Practicle_Task/Edvora_Practicle_Task/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Edvora_Practicle_Task
+//
+//  Created by Manan Kakadiya on 24/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Edvora_Practicle_Task/Edvora_Practicle_Task/Edvora_Practicle_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edvora_Practicle_TaskApp.swift
+//  Edvora_Practicle_Task
+//
+//  Created by Manan Kakadiya on 24/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Edvora_Practicle_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
